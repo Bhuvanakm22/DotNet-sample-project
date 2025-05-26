f50cd245b3d603ae616be55479fa1a25c41dd2dd
Bhuvanakm22 <bhuvana.kandasamy.uk@gmail.com>
2025-05-26 20:33:41 +0100

    command to generate constructor

--- WpfApp1/WpfApp1/Collections.cs
+++ WpfApp1/WpfApp1/Collections.cs
@@ -8,6 +8,29 @@ namespace NetConceptWithWpfApp
 {
     internal class Collections
     {
+
+        public string Name;
+       public string FileName;
+        public int Count;
+        public List<string> Items;
+
+        //Used "ctor" command to generate constructor by using VS IDE
+        public Collections()
+        {
+            
+        }
+        //1. Place your cursor in any empty line in a class;
+        //2. Press Ctrl + . to trigger the Quick Actions and Refactorings menu;
+        public Collections(string name, string fileName, int count, List<string> items)
+        {
+            Name = name;
+            FileName = fileName;
+            Count = count;
+            Items = items;
+        }
+
+
+
         /// <summary>
         /// the "yield" keyword allows you to create iterator methods, which are methods 
         /// that return an IEnumerable or IEnumerator interface. It enables you to generate a sequence of values one at a time, 
